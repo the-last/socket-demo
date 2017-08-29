dd06329038286cec3ceca093e901a4d93c8cad03
GaoYaBing <gaoyabing636@pingan.com.cn>
2017-08-29 09:11:33 +0800

    update

---
+++ expressdemo.js
@@ -0,0 +1,19 @@
+﻿var path = require('path');
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+server.listen(4000, function(){
+    console.log('waiting')
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket) {
+    socket.on('event', function(data) {
+    	console.log(data);
+        socket.emit('news', {respones: '对呀！' + "你发送的是 : " + data.dt});
+    });
+});
